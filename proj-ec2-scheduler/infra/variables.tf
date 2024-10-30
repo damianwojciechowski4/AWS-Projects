2e581749f994d34e686bee0ac405518f631a90be
damianwojciechowski4 <110458027+damianwojciechowski4@users.noreply.github.com>
2024-10-30 19:04:05 +0100

    Update variables.tf

--- proj-ec2-scheduler/infra/variables.tf
+++ proj-ec2-scheduler/infra/variables.tf
@@ -6,20 +6,25 @@ variable "aws_region" {
 }
 
 variable "environment" {
+  description = "Name of your environment"
   type    = string
   default = "dev"
 }
 
 variable "EventBridge_ScheduleName" {
+  description = "Name of EventBridge Scheduler"
   type = string
   default = "checkEC2HourlyState-schedule"
 }
 
 variable "aws_dynamoDB_tableName" {
+    description = "Name of DynamoDB table"
     type = string
     default = "ec2-scheduler-table"
 }
 
 variable "lambda_function_name" {
+  description = "Name of Lambda Function"
+  type = string
   default = "EC2SchedulerLambda"
 }
