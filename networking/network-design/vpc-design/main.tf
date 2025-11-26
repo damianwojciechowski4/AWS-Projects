64f627be62d7bada57da5627c9d46dab1fd02b65
Damian Wojciechowski <damian.wojciechowski@ergo.com>
2025-11-26 18:20:11 +0100

    updated terraform main.tf

--- networking/network-design/vpc-design/main.tf
+++ networking/network-design/vpc-design/main.tf
@@ -36,12 +36,17 @@ variable "aws_region" {
 resource "aws_s3_bucket" "example" {
   bucket = "my-test-bucket-${random_id.suffix.hex}"
   tags = {
-    Name        = "My bucket"
-    Environment = "Bucket"
+    Name        = "test-bucket-terraform-CICD-pipeline"
+    Environment = "PROD"
     IaC = "Terraform"
   }
 }
 
 resource "random_id" "suffix" {
   byte_length = 4
+}
+
+output "bucket_name" {
+  description = "S3 bucket name."
+  value       = aws_s3_bucket.example.id
 }
