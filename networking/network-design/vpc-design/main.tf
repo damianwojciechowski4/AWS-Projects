ca9c64d0cefb398595dc46435b1627d58cacde33
Damian Wojciechowski <damian.wojciechowski@ergo.com>
2025-11-26 18:12:50 +0100

    updated terraform state

---
+++ networking/network-design/vpc-design/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+        source  = "hashicorp/random"
+        version = "~> 3.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = var.aws_region
+}
+
+
+
+backend "s3" {
+  bucket = "terraform-state-aws-projects-network"
+  key    = "networking/network-design/vpc-design/state/terraform.tfstate"
+  region = "eu-central-1"
+}
+
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-test-bucket-${random_id.suffix.hex}"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
