9d6a93985625b7d93be7a8b662c260f7813a2334
Damian Wojciechowski <damian.wojciechowski@ergo.com>
2025-12-22 17:39:50 +0100

    corrected module refference

--- networking/network-design/vpc-design/envs/prod/main.tf
+++ networking/network-design/vpc-design/envs/prod/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 module "vpc" {
   source = "../../modules/vpc"
 
-  vpc_name        = var.name
+  name        = var.name
   vpc_cidr        = var.vpc_cidr
   public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
