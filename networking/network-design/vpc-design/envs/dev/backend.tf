d7bb913896110d8ed9b3fc2010abc0142b16fbab
Damian Wojciechowski <damian.wojciechowski@ergo.com>
2025-12-22 17:41:54 +0100

    update

--- networking/network-design/vpc-design/envs/dev/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-aws-projects-network"
-    key    = "networking/network-design/vpc-design/state/terraform.tfstate"
-    region = "eu-central-1"
-  }
-}
