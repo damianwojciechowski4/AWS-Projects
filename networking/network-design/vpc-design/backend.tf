c7e53b2c3b811d095589aec7c54860c83806decc
Damian Wojciechowski <damian.wojciechowski@ergo.com>
2025-12-22 15:41:49 +0100

    updated lockfile

--- networking/network-design/vpc-design/backend.tf
+++ networking/network-design/vpc-design/backend.tf
@@ -1,6 +1,10 @@
-backend "s3" {
+terraform { 
+    backend "s3" {
     bucket = "terraform-state-aws-projects-network"
     key    = "networking/network-design/vpc-design/state/terraform.tfstate"
     region = "eu-central-1"
     use_lockfile = true
   }
+
+
+}
