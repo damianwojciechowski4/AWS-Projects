2027ae152c5626f8d1d1604a325d362ea08afb01
Damian Wojciechowski <damianwojciechowski04@gmail.com>
2026-02-03 22:04:07 +0100

    updated modules with spoke and inspection vpc, enable_internet_gateway, enable_nat_gateway

--- networking/network-design/vpc-design/envs/dev/outputs.tf
+++ networking/network-design/vpc-design/envs/dev/outputs.tf
@@ -1,22 +1,35 @@
-output "vpc_id" {
-  description = "A map of VPC names to their IDs"
+output "inspection_vpc_id" {
+  description = "Inspection VPC ID"
+  value       = module.inspection_vpc["inspection"].vpc_id
+  }
+
+output "spoke_vpc_id" {
+  description = "Spoke VPC IDs"
   value = {
-    for k, v in module.vpc :
-    k => v.vpc_id
+    for name, vpc_mod in module.spoke_vpc :
+    name => vpc_mod.vpc_id
   }
 }
 
-output "private_subnet_ids" {
+output "inspection_private_subnet_ids" {
   description = "Private Subnet IDs for each VPC"
   value = {
-    for name, vpc_mod in module.vpc :
+    for name, vpc_mod in module.inspection_vpc:
     name => vpc_mod.private_subnet_ids
   }
 }
-output "public_subnet_ids" {
+output "inspectionpublic_subnet_ids" {
   description = "Public Subnet IDs for each VPC"
   value = {
-    for name, vpc_mod in module.vpc :
+    for name, vpc_mod in module.inspection_vpc:
     name => vpc_mod.public_subnet_ids
+  }
+}
+
+output "spoke_private_subnet_ids" {
+  description = "Private subnets for spoke VPCs"
+  value = {
+    for name, vpc_mod in module.spoke_vpc:
+    name => vpc_mod.private_subnet_ids
   }
 }
